1901a31ec860d583ade85e070042914ad38e9b82
errolcostabir <errolcostabir98@gmail.com>
2021-05-21 17:42:02 +0530

    deploying again

--- src/About.js
+++ src/About.js
@@ -57,21 +57,21 @@ class About extends Component {
                     <h2 className="h2-heading">Tech Stack/Programming languages I work with...</h2>
                     <br />
                     <div className="row">
-                        <img src={c} className="icon-holder col" />
-                        <img src={cpp} className="icon-holder col" />
-                        <img src={java} className="icon-holder col" />
-                        <img src={js} className="icon-holder col" />
-                        <img src={python} className="icon-holder col" />
-                        <img src={php} className="icon-holder col" />
+                        <img src={c} alt="..." className="icon-holder col" />
+                        <img src={cpp} alt="..." className="icon-holder col" />
+                        <img src={java}  alt="..." className="icon-holder col" />
+                        <img src={js} alt="..." className="icon-holder col" />
+                        <img src={python} alt="..." className="icon-holder col" />
+                        <img src={php} alt="..." className="icon-holder col" />
                     </div>
                     <br />
                     <div className="row">
-                        <img src={reactjs} className="icon-holder col" />
-                        <img src={node} className="icon-holder col" />
-                        <img src={flutter} className="icon-holder col" />
-                        <img src={html} className="icon-holder col" />
-                        <img src={css} className="icon-holder col" />
-                        <img src={sql} className="icon-holder col" />
+                        <img src={reactjs} alt="..." className="icon-holder col" />
+                        <img src={node} alt="..." className="icon-holder col" />
+                        <img src={flutter} alt="..." className="icon-holder col" />
+                        <img src={html} alt="..." className="icon-holder col" />
+                        <img src={css} alt="..." className="icon-holder col" />
+                        <img src={sql} alt="..." className="icon-holder col" />
                     </div>
                 </div>
                 <hr />
--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link, Redirect } from 'react-router-dom';
+//import { Link, Redirect } from 'react-router-dom';
 import Navbar from './Navbar';
 import './App.css';
 import errol from './resources/errol.jpg';
