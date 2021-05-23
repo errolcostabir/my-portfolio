66edf27a54d78a51155fe9296af36e81f04e1e88
errolcostabir <errolcostabir98@gmail.com>
2021-05-23 11:40:03 +0530

    fixing techstack again 2

--- src/About.js
+++ src/About.js
@@ -51,27 +51,25 @@ class About extends Component {
                 </div>
                 <br />
                 <br />
-                <div className="row card">
-                    <h2 className="h2-heading">My Techstack</h2>
+                <h2 className="h2-heading">My Techstack</h2>
+                <br />
+                <div className="row">
+                    <div className="row">
+                        <img src={c} alt="..." className="icon-holder col" />
+                        <img src={cpp} alt="..." className="icon-holder col" />
+                        <img src={java} alt="..." className="icon-holder col" />
+                        <img src={js} alt="..." className="icon-holder col" />
+                        <img src={python} alt="..." className="icon-holder col" />
+                        <img src={php} alt="..." className="icon-holder col" />
+                    </div>
                     <br />
-                    <div className="card-body">
-                        <div className="row">
-                            <img src={c} alt="..." className="icon-holder col" />
-                            <img src={cpp} alt="..." className="icon-holder col" />
-                            <img src={java} alt="..." className="icon-holder col" />
-                            <img src={js} alt="..." className="icon-holder col" />
-                            <img src={python} alt="..." className="icon-holder col" />
-                            <img src={php} alt="..." className="icon-holder col" />
-                        </div>
-                        <br />
-                        <div className="row">
-                            <img src={reactjs} alt="..." className="icon-holder col" />
-                            <img src={node} alt="..." className="icon-holder col" />
-                            <img src={flutter} alt="..." className="icon-holder col" />
-                            <img src={html} alt="..." className="icon-holder col" />
-                            <img src={css} alt="..." className="icon-holder col" />
-                            <img src={sql} alt="..." className="icon-holder col" />
-                        </div>
+                    <div className="row">
+                        <img src={reactjs} alt="..." className="icon-holder col" />
+                        <img src={node} alt="..." className="icon-holder col" />
+                        <img src={flutter} alt="..." className="icon-holder col" />
+                        <img src={html} alt="..." className="icon-holder col" />
+                        <img src={css} alt="..." className="icon-holder col" />
+                        <img src={sql} alt="..." className="icon-holder col" />
                     </div>
                 </div>
                 <hr></hr>
